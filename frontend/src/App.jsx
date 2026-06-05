308c645ba874a2f7b141a96f9836c3c04627dfaa
UsamaArif <malik.usamarif@gmail.com>
2026-06-05 21:46:51 +0500

    Set up routing for application with protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,34 @@
-
+import { Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage      from './pages/LandingPage'
+import LoginPage        from './pages/LoginPage'
+import DashboardLayout  from './layouts/DashboardLayout'
+import DashboardHome    from './pages/dashboard/DashboardHome'
+import UploadScan       from './pages/dashboard/UploadScan'
+import TokenDemo        from './pages/dashboard/TokenDemo'
+import ApiKeys          from './pages/dashboard/ApiKeys'
+import UserManagement   from './pages/dashboard/UserManagement'
+import ProtectedRoute   from './components/ProtectedRoute'
+ 
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/"      element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+ 
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <DashboardLayout />
+        </ProtectedRoute>
+      }>
+        <Route index         element={<DashboardHome />} />
+        <Route path="upload" element={<UploadScan />} />
+        <Route path="tokens" element={<TokenDemo />} />
+        <Route path="api-keys" element={<ApiKeys />} />
+        <Route path="users"  element={<UserManagement />} />
+      </Route>
+ 
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
+ 
